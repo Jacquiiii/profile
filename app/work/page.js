8e547a26787e5cdde6b85c53e0cc0b30b4bfec1c
Jacquiiiii <koroll.jacqui@gmail.com>
2024-01-29 22:16:45 -0800

    feat: add project to work page

--- app/work/page.js
+++ app/work/page.js
@@ -3,7 +3,15 @@ import styles from './work.module.css'
 const Work = () => {
   return (
     <div className={styles.work}>
-      Work
+      <div className={styles.project}>
+        <a href="https://admin-dashboard-books.vercel.app/" target="_blank">
+          <img className={styles.image} src="/admin_dashboard.png" />
+        </a>
+        <div className={styles.projectDetails}>
+          <h2><a href="https://github.com/Jacquiiiii/Admin-Dashboard" target="_blank">Admin Dashboard</a></h2>
+          <span>Add description and highlights</span>
+        </div>
+      </div>
     </div>
   )
 }
